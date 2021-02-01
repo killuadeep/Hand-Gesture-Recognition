5378e70bc633db45043f353ca187a2a0a11d9834
killuadeep <77839791+killuadeep@users.noreply.github.com>
2021-02-01 10:28:25 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,19 @@
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (IMG_SIZE,IMG_SIZE,1)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(10, activation = "softmax"))
